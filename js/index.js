b0b2aedda78d1bf282ee3436394068c54d55ce0a
Eduardo Fabián <edgar.herrera@totvs.com>
2022-06-17 17:14:19 -0500

    [FV-01] Se agrega la función Ajax getUserByName desde FV01

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 function saludo(){
 	alert("Hola mundo");
 }
+
+function getUserByName(username, password){
+	$.ajax(
+	{
+		url:'',
+		method:'POST',
+		data:{
+			username: username,
+			password: password
+		}, function beforeSend(){
+		}, function success(data){
+			console.log(data);
+		}, function error(ex) {
+			console.log(ex);
+		}
+	});
+
+}
